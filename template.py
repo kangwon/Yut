1a63237e7ca28d166e66af654760ef9851785d93
kangwon1260@gmail.com <kang_won@naver.com>
2018-04-21 00:16:44 +0900

    디버그용 함수 추가

--- template.py
+++ template.py
@@ -34,6 +34,12 @@ class Template:
         for user in users:
             print(f'{user}: [{", ".join([str(p) for p in user.staying_players])}] / [{", ".join([str(p) for p in user.goaled_players])}]')
 
+    # for debug
+    def print_companies(self, users):
+        for u in users:
+            for p in u.players:
+                print(p, 'companies', p.company)
+
 
 """
 o . . . . o . . . . o . . . . o . . . . o . . . . o
